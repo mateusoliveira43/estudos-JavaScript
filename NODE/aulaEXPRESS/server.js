2a53b78fcf630472a4153992e3645dbbe93ba239
Mateus Oliveira <matews1943@gmail.com>
2020-09-23 14:43:59 -0300

    seçao 10: segurança

--- NODE/aulaEXPRESS/server.js
+++ NODE/aulaEXPRESS/server.js
@@ -1,35 +1,39 @@
-// CRUD: Create (POST), Read (GET), Update (PUT) e Delete (DELETE)
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
 mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.emit('pronto');
   })
   .catch(e => console.log(e));
-
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const flash = require('connect-flash');
-
 const routes = require('./routes');
 const path = require('path');
-const meuMiddleware = require('./src/middlewares/middleware');
+const helmet = require('helmet');
+const csrf = require('csurf');
+const { meuMiddleware, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+// app.use(helmet());
 
 app.use(express.urlencoded({ extended: true, }));
-
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
-const sessionOptions = session ({
-  secret: 'ashjadghdsghaghgksakgsasdgdgshaashhagskgsakhkg',
-  store: new MongoStore({mongooseConnection: mongoose.connection}),
+const sessionOptions = session({
+  secret: 'ashjadghdsghaghgksakgsasdgdgshaashhagskgsakhkg fsts ddgtsttfadd',
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
   resave: false,
   saveUninitialized: false,
   cookie: {
-    maxAge: 1000* 60*60*24*7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
     httpOnly: true
   }
 });
@@ -39,26 +43,16 @@ app.use(flash());
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
+app.use(csrf());
 // Nossos próprios middlewares
 app.use(meuMiddleware);
-
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
 app.use(routes);
 
 app.on('pronto', () => {
   app.listen(3000, () => {
     console.log('Acessar http://localhost:3000');
-    console.log('servidor executando na porta 3000');
+    console.log('Servidor executando na porta 3000');
   });
 });
-
-// app.get('/testes/:idUsusarios?/:parametro?', (req, res) => {
-//   console.log(req.params);
-//   console.log(req.query);
-//   // res.send(req.params);
-//   res.send(req.query);
-// });
-
-// app.post('/', (req, res) => {
-//   console.log(req.body);
-//   res.send(`Recebi o formulário: ${req.body.outracoisa}`);
-// });
