5375bc260b322f23ad36fcc6b5075ee588a228ba
Mateus Souza Oliveira <matews1943@gmail.com>
2020-12-30 16:20:55 -0300

    api rest: rotas de alunos

--- api_rest/src/controllers/HomeController.js
+++ api_rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Mateus',
-      sobrenome: 'Oliveira',
-      email: 'email@email.com',
-      idade: 24,
-      peso: 80.5,
-      altura: 1.80,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
