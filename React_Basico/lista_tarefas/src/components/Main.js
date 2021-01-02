219bccaa387673acf98c80f613a2b7523b9faf0b
Mateus Souza Oliveira <matews1943@gmail.com>
2021-01-02 13:53:30 -0300

    react basico: editando tarefas

--- React_Basico/lista_tarefas/src/components/Main.js
+++ React_Basico/lista_tarefas/src/components/Main.js
@@ -12,21 +12,32 @@ export default class Main extends Component {
   state = {
     novaTarefa: '',
     tarefas: [],
+    index: -1,
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { tarefas } = this.state;
+    const { tarefas, index } = this.state;
     let { novaTarefa } = this.state;
     novaTarefa = novaTarefa.trim();
 
     if (tarefas.indexOf(novaTarefa) !== -1) return;
 
     const novasTarefas = [...tarefas];
+    if (index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: '',
+      });
+    } else {
+      novasTarefas[index] = novaTarefa;
 
-    this.setState({
-      tarefas: [...novasTarefas, novaTarefa],
-    });
+      this.setState({
+        tarefas: [...novasTarefas],
+        novaTarefa: '',
+        index: -1,
+      });
+    }
   }
 
   handleChange = (e) => {
@@ -36,6 +47,11 @@ export default class Main extends Component {
   }
 
   handleEdit = (e, index) => {
+    const { tarefas } = this.state;
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
   }
 
   handleDelete = (e, index) => {
