7699d298a5d664c3e2bd9ed15983dcf497be9c63
Mateus Souza Oliveira <matews1943@gmail.com>
2021-01-02 11:00:10 -0300

    react basico: estilizando as tarefas

--- React_Basico/lista_tarefas/src/components/Main.js
+++ React_Basico/lista_tarefas/src/components/Main.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 
 // Form
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaEdit, FaWindowClose } from 'react-icons/fa';
+
+// Form
+// import { FaEdit, FaWindowClose } from 'react-icons/fa';
 
 import './Main.css';
 
 export default class Main extends Component {
   state = {
     novaTarefa: '',
+    tarefas: [
+      'Fazer exercício',
+      'Estudar',
+      'Ler',
+    ],
   };
 
   handleChange = (e) => {
@@ -17,7 +25,7 @@ export default class Main extends Component {
   }
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className="main">
@@ -33,6 +41,18 @@ export default class Main extends Component {
             <FaPlus />
           </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
