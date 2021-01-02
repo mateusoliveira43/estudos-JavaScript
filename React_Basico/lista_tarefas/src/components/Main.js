9cfc79ad728a2e4627034301e1c0e265e257260a
Mateus Souza Oliveira <matews1943@gmail.com>
2021-01-01 22:03:18 -0300

    react basico: componentes

---
+++ React_Basico/lista_tarefas/src/components/Main.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+export default class Main extends Component {
+  state = {
+    novaTarefa: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  }
+
+  render() {
+    const { novaTarefa } = this.state;
+
+    return (
+      <div className="main">
+        <h1>Lista de tarefas</h1>
+
+        <form action="#">
+          <input onChange={this.handleChange} type="text" />
+          <button type="submit">Enviar</button>
+        </form>
+
+        <p>{novaTarefa}</p>
+      </div>
+    );
+  }
+}
