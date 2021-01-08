c9a11f1c035f3885ca2a8cf406f7784047df7710
Mateus Souza Oliveira <matews1943@gmail.com>
2021-01-07 21:11:01 -0300

    react+redux: configurando as cores

--- React_Basico/react-base/src/styles/GlobalStyles.js
+++ React_Basico/react-base/src/styles/GlobalStyles.js
@@ -1,28 +1,37 @@
 import styled, { createGlobalStyle } from 'styled-components';
+import { primaryColor, primaryDarkColor } from '../config/colors';
 
 export default createGlobalStyle`
   *{
-    margin:0;
-    padding:0;
-    outline:0;
-    box-sizing:border-box;
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
   }
 
   body {
-    font-family:sans-serif;
-    background: #eee;
+    font-family: sans-serif;
+    background: ${primaryDarkColor};
+    color: ${primaryDarkColor};
   }
 
   html, body, #root {
-    height:100%;
+    height: 100%;
   }
 
   button {
     cursor: pointer;
+    background: ${primaryColor};
+    border:none;
+    color: #fff;
+    padding:10px 20px;
+    border-radius: 4px;
+    font-weight: 700;
   }
 
   a {
     text-decoration: none;
+    color: ${primaryColor};
   }
 
   ul {
