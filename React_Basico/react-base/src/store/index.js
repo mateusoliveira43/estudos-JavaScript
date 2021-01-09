c1f1a58fc7bc032e9e57759d095ef1a57de3f9ee
Mateus Souza Oliveira <matews1943@gmail.com>
2021-01-09 15:54:16 -0300

    react+redux: redux persist

--- React_Basico/react-base/src/store/index.js
+++ React_Basico/react-base/src/store/index.js
@@ -1,13 +1,20 @@
+import { persistStore } from 'redux-persist';
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+
+import persistedReducers from './modules/reduxPersist';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const strore = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const strore = createStore(
+  persistedReducers(rootReducer),
+  applyMiddleware(sagaMiddleware)
+);
 
 sagaMiddleware.run(rootSaga);
 
+export const persistor = persistStore(strore);
 export default strore;
