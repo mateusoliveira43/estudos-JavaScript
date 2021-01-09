78a160074483a28e2bb995f169b58a811a122ef0
Mateus Souza Oliveira <matews1943@gmail.com>
2021-01-09 09:56:20 -0300

    react+redux: configurando o axios

--- React_Basico/react-base/src/pages/Login/index.js
+++ React_Basico/react-base/src/pages/Login/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { toast } from 'react-toastify';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
 
+import axios from '../../services/axios';
+
 export default function Login() {
-  toast.success('Oi, sucesso :)');
-  toast.error('Tchau, erro :(');
+  React.useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/alunos');
+      const { data } = response;
+      console.log(data);
+    }
+
+    getData();
+  }, []);
 
   return (
     <Container>
