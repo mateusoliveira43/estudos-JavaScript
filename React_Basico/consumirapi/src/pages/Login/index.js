14ed2e510c2783c6fde204af74f73450f28c477c
Mateus Souza Oliveira <matews1943@gmail.com>
2021-01-10 14:24:25 -0300

    react+redux: login

--- React_Basico/consumirapi/src/pages/Login/index.js
+++ React_Basico/consumirapi/src/pages/Login/index.js
@@ -1,11 +1,59 @@
 import React from 'react';
+import { toast } from 'react-toastify';
+import { isEmail } from 'validator';
+import { useDispatch } from 'react-redux';
+import { get } from 'lodash';
 
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
+import * as actions from '../../store/modules/auth/actions';
 
-export default function Login() {
+export default function Login(props) {
+  const dispatch = useDispatch();
+
+  const prevPath = get(props, 'location.state.prevPath', '/');
+
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let formErros = false;
+
+    if (!isEmail(email)) {
+      formErros = true;
+      toast.error('E-mail deve ser válido');
+    }
+
+    if (password.length < 6 || password.length > 50) {
+      formErros = true;
+      toast.error('Senha inválida');
+    }
+
+    if (formErros) return;
+
+    dispatch(actions.loginRequest({ email, password, prevPath }));
+  };
+
   return (
     <Container>
       <h1>Login</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Seu e-mail"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Sua senha"
+        />
+        <button type="submit">Acessar</button>
+      </Form>
     </Container>
   );
 }
