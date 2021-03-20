e0fbc065c1dd65d3f0bc60bd77bd10d39451fd70
Mateus Oliveira <matews1943@gmail.com>
2021-03-20 15:48:07 -0300

    TypeScript: POO (continuacao)

--- Aulas_TypeScript/src/poo.ts
+++ Aulas_TypeScript/src/poo.ts
@@ -27,3 +27,60 @@ console.log(colaborador1);
 empresa.adicionaColaborador(colaborador1);
 empresa.adicionaColaborador(colaborador2);
 console.log(empresa);
+
+export class Pessoa {
+  constructor(
+    public nome: string,
+    public sobrenome: string,
+    private idade: number,
+    protected _cpf: string,
+  ) {
+    this.cpf = _cpf; // chamar o setter na inicialização
+  }
+
+  getIdade(): number {
+    return this.idade;
+  }
+
+  set cpf(cpf: string) {
+    this._cpf = cpf;
+  }
+
+  get cpf(): string {
+    return this._cpf;
+  }
+
+  getNomeCompleto(): string {
+    return `${this.nome} ${this.sobrenome}`;
+  }
+
+  static falaOi(): void {
+    console.log('oi');
+  }
+
+  static oi = 'oi';
+}
+
+export class Aluno extends Pessoa {
+  constructor(
+    nome: string,
+    sobrenome: string,
+    idade: number,
+    cpf: string,
+    public sala: string,
+  ) {
+    super(nome, sobrenome, idade, cpf);
+  }
+
+  getNomeCompleto(): string {
+    console.log('fazendo algo antes...');
+    return super.getNomeCompleto();
+  }
+}
+export class Cliente extends Pessoa {}
+
+const aluno = new Aluno('Mateus', 'Oliveira', 25, '000.000.000-00', 'A-37');
+console.log(aluno, aluno.getNomeCompleto());
+aluno.cpf = 'troquei no setter';
+console.log(aluno.cpf);
+Aluno.falaOi();
